46b096710ffd7cfb03fe81ae10278703a03f183e
michelbaratella <baratella2@gmail.com>
2026-07-02 19:03:12 -0300

    feat: working v1, coming soon!

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,13 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Michel's Portfolio" },
+    { name: "description", content: "Welcome to my Github page!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  // return <Welcome />;
+  return <p>Coming soon...</p>
 }
