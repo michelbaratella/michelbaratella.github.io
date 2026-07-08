8a49f858c2b5c53937f82f2bade8a67e7a4bfd72
michelbaratella <baratella2@gmail.com>
2026-07-08 15:14:37 -0300

    fix: layout base rendering method

--- app/routes.ts
+++ app/routes.ts
@@ -1,5 +1,8 @@
-import { type RouteConfig, route } from "@react-router/dev/routes";
+import { type RouteConfig, layout, route } from "@react-router/dev/routes";
 
 export default [
-  route("/", "./routes/home.tsx", [route("profile", "./routes/profile.tsx")]),
+  layout("./components/AppShellContent.tsx", [
+    route("/", "./routes/home.tsx"),
+    route("profile", "./routes/profile.tsx"),
+  ]),
 ] satisfies RouteConfig;
