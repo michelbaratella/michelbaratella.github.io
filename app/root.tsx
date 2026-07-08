dfc84ce2429de5d50273b75e82966b907101a28f
michelbaratella <baratella2@gmail.com>
2026-07-08 14:53:39 -0300

    fix: favicon path

--- app/root.tsx
+++ app/root.tsx
@@ -35,7 +35,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" type="image/svg+xml" href="public/favicon.svg" />
+        <link rel="icon" type="image/svg+xml" href="favicon.svg" />
         <ColorSchemeScript />
         <Meta />
         <Links />
